844b18ae8df46256c80c3f512c93d042fdfc5b96
lete114 <lete@lete114.top>
2021-08-10 19:24:58 +0800

    Version1.0.0

---
+++ ForIcon/main.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ForIcon
+{
+    partial class ForIcon
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForIcon));
+            this.msg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msg
+            // 
+            this.msg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.msg.Font = new System.Drawing.Font("宋体", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.msg.ForeColor = System.Drawing.Color.Red;
+            this.msg.Location = new System.Drawing.Point(12, 9);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(258, 235);
+            this.msg.TabIndex = 0;
+            this.msg.Text = "软件拖入...";
+            this.msg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ForIcon
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.msg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ForIcon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "";
+            this.Text = "获取软件ico图标";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.ForIcon_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.ForIcon_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msg;
+
+    }
+}
+
---
+++ ForIcon/main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForIcon
+{
+    public partial class ForIcon : Form
+    {
+        public ForIcon()
+        {
+            InitializeComponent();
+        }
+
+
+        // 拖入文件执行
+        private void ForIcon_DragEnter(object sender, DragEventArgs e){
+                e.Effect = DragDropEffects.All;
+        }
+
+        // 拖入文件松开鼠标后执行
+        private void ForIcon_DragDrop(object sender, DragEventArgs e)
+        {
+            // 获取拖入的文件路径
+            string File_Path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            // 判断后缀名
+            if (System.IO.Path.GetExtension(File_Path) != ".exe") {
+                MessageBox.Show("当前文件不是EXE文件~", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } else {
+                SaveFileDialog sfd = new SaveFileDialog(); // 保存文件对话框
+                sfd.Filter = "|*.ico"; // 后缀名
+                if (sfd.ShowDialog() == DialogResult.OK) { // 判断是否点击保存(未填写文件名则无效)
+                    // 获取拖入文件的图标
+                    Image img = Icon.ExtractAssociatedIcon(File_Path).ToBitmap();
+                    img.Save(sfd.FileName);
+                } 
+            }
+        }
+    }
+}
